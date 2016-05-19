d5a0c9f80348b885cacaed886b44662f610928a1
9Folken <paulbykow@list.ru>
2016-05-19 15:35:40 +0800

    add_model_battle

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160414030634) do
+ActiveRecord::Schema.define(version: 20160519072407) do
+
+  create_table "battles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "units", force: :cascade do |t|
     t.string   "name"
